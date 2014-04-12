d59802e060e811ff06a54573498ce31bb968a114
Kyle Blagg <noahjemkai@gmail.com>
2014-04-12 00:15:48 -0400

    minor bug fixes and alterations

--- toolbar.php
+++ toolbar.php
@@ -18,12 +18,12 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Project name</a>
+      <a class="navbar-brand" href="#">CS405G Widgets and Wingdings Store</a>
     </div>
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
+        <li class="active"><a href="index.php">Welcome Page</a></li>
+        <li><a href="items.php">Items</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
     </div><!--/.nav-collapse -->
