a5af3d0b24bad61cddf2a245cf3376fcf8576ed9
Kyle Blagg <noahjemkai@gmail.com>
2014-04-17 01:57:23 -0400

    Bug fixes for global scoping issues;

--- item.php
+++ item.php
@@ -11,46 +11,13 @@ include 'item-ops.php';
 $isn = $_GET['isn'];
 $item = get_item_by_isn($isn);
 
-function order_item($isn,$err){
-    echo "<form action='add_to_cart.php' method='POST'>";
-    if ($err == '1'){
-        echo "<tr><td colspan='2'><input type='text' name='quantity' size='6'"
-        . "style='background-color: #df0078;'>"
-                . "<font style='color:red;'>Insufficient Inventory</font>";
-    }else{
-        echo "<tr><td colspan='2'><input type='text' name='quantity' size='10'>";
-    }
-    echo "<button type='submit' name='isn' value='" . $isn . "'>Add to Cart</button>"
-        . "</td></tr></form>";
-}
-
-function displayItemDetails(){
-    $item = get_item_by_isn($_GET['isn']);
-    echo "<table>"
-        . "<tr>"
-            . "<th colspan='2'><h3>" . $item['name'] . "</h3></th>"
-        . "</tr>"
-        . "<tr>"
-            . "<td>Price:</td>"; 
-    if ($item['promotion'] > 0) {
-            promoValue($item);
-    echo "</tr>"
-        . "<tr><td>Sale:</td><td><font style='color:green'>" 
-              . strval(number_format($item['promotion']*100,2)) . "% off!</font></td></tr>";}
-    else{
-         echo "<td>" . strval($item['price']) . "</td></tr>";
-        }
-    echo "<tr><td>Number in Stock:</td><td>" . strval($item['quantity']) . "</td></tr>";
-       order_item($_GET['isn'],$_GET['error']);
-    echo "<tr><td>Description:</td><td>" . $item['description'] . "</td></tr>";
-    echo "<tr><td>ISN:</td><td>" . $item['isn'] . "</td></tr>";
-    echo "</table>";
-}
 function itemName(){
+    global $item;
     echo $item['name'];
 }
 
 function itemPrice(){
+    global $item;
     if ($item['promotion'] > 0){
         promoValue($item);
     }else{
@@ -59,6 +26,7 @@ function itemPrice(){
 }
 
 function itemDiscount(){
+    global $item;
     if ($item['promotion'] > 0){
         echo "<font style='color:green'>" 
               . strval(number_format($item['promotion']*100,2))
@@ -69,14 +37,17 @@ function itemDiscount(){
 }
 
 function itemQty(){
+    global $item;
     echo $item['quantity'];
 }
 
 function itemDescription(){
+    global $item;
     echo $item['description'];
 }
 
 function itemISN(){
+    global $item;
     echo $item['isn'];
 }
 ?>
