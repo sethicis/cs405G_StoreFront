b3bbf360280fd1083d1102d04228ba2114fe4298
Kyle Blagg <noahjemkai@gmail.com>
2014-04-17 05:43:50 -0400

    Minor changes to UI of Order_details.php
    Ready to begin work on Staff point of view

--- order_details.php
+++ order_details.php
@@ -12,7 +12,7 @@ include 'query.php';
 function get_order_items(){
     $orderID = $_GET['order'];
     $order = get_ordered_items($orderID);
-    $sum = 0;
+    $sum = 0; $count = 0;
     while ($row = mysqli_fetch_array($order)){
         echo "<tr>";
             echo "<td>"
@@ -23,11 +23,17 @@ function get_order_items(){
             echo "<td>\$" . strval(($row['price'] - $row['promotion']*$row['price'])*$row['quantity']) . "</td>";
         echo "</tr>";
         $sum += ($row['price'] - $row['promotion']*$row['price'])*$row['quantity'];
+        $count += ($row['quantity']);
     }
     
     echo "<tr>
-            <td>---</td><td>---</td><td>---</td><td>---</td><td>\$${sum}</td>
+            <td>---</td><td>---</td><td>---</td><td>${count}</td><td>\$${sum}</td>
          </tr>";
+}
+
+function tableTitle(){
+    $orderID = $_GET['order'];
+    echo "<h3>Order# " . $orderID . "</h3>";
 }
 head("Order Details");
 ?>
@@ -35,6 +41,11 @@ head("Order Details");
 <body>
     <?php include 'toolbar.php'; ?>
     <div class='container'>
+        <div class='row'>
+            <div class='col-lg-12 text-center'>
+                <?php tableTitle() ?>
+            </div>
+        </div>
         <div class='row'>
             <div class='col-lg-12'>
                 <table class='table table-striped'>
