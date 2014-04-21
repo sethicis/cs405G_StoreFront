94b94b382c1657a46ab69d3bf617b73c0cd1bbc8
Kyle Blagg <noahjemkai@gmail.com>
2014-04-21 19:47:29 -0400

    Fixed issue with search, additional styling fixes

--- search_handle.php
+++ search_handle.php
@@ -9,33 +9,37 @@ include_once 'query.php';
  * and open the template in the editor.
  */
 
-    if (isset($_POST['term']))
-	//if (TRUE)
-    {
-        //Search for term
-        $term = $_POST['term']; //Get the name of the variable to search for
-        //$term = 'wingding1';
-	//Feature, add support to search by not just name, but isn as well.
-        //Get the details for a particular item
-        $item = get_item($term);
-        if ($item != NULL){
-            header("Location: item.php?isn=" . $item['isn'] . "&error=0");
-		exit;
-        }else{
-            //TODO: Handle search term with no hits
-        }
-    }
-    else
-    {
-        //Browse all items
-        //$items = get_all_items();
-	head('Browse Items');
-	echo "<body>";
+if (isset($_POST['term']))
+{
+    //Search for term
+    $term = $_POST['term']; //Get the name of the variable to search for
+    //Feature, add support to search by not just name, but isn as well.
+    //Get the details for a particular item
+    $item = get_item($term);
+    if ($item != NULL){
+        header("Location: item.php?isn=" . $item['isn'] . "&error=0");
+        exit;
+    }else{
+        head('Search Item not Found');
+        echo "<body>";
         include 'toolbar.php';
         echo "<p>Blank Page...</p>";
         include 'footer.php';
-	echo "</body>";
+        echo "</body>";
+        echo "</html>";
     }
-
+}
+else
+{
+    //Browse all items
+    //$items = get_all_items();
+    head('Search Item not found');
+    echo "<body>";
+    include 'toolbar.php';
+    echo "<p>Blank Page...</p>";
+    include 'footer.php';
+    echo "</body>";
+    echo "</html>";
+}
 
 ?>
